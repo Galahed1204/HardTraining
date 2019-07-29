e1fe7e75733c19cceb873064f67b9f2768f5aaef
Валентин <galahed1205@gmail.com>
2019-07-29 18:21:07 +1000

    room test1

---
+++ app/src/main/java/com/galinc/hardtraining/db/ExerciseDao.java
@@ -0,0 +1,33 @@
+package com.galinc.hardtraining.db;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.galinc.hardtraining.itility.Exercise;
+
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.List;
+
+
+@Dao
+public interface ExerciseDao {
+
+    @Query("SELECT * FROM Exercise")
+    List<Exercise> getAll();
+
+    @Query("SELECT * FROM Exercise WHERE id = :id")
+    Exercise getById(long id);
+
+    @Insert
+    void insert(Exercise employee);
+
+    @Update
+    void update(Exercise employee);
+
+    @Delete
+    void delete(Exercise employee);
+}
--- app/src/main/java/com/galinc/hardtraining/itility/Exercise.java
+++ app/src/main/java/com/galinc/hardtraining/itility/Exercise.java
@@ -1,12 +1,19 @@
 package com.galinc.hardtraining.itility;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+@Entity
 public class Exercise {
     @SerializedName("title")
     @Expose
     private String title;
+
+    @PrimaryKey(autoGenerate = true)
+    public long id;
 
     public String getTitle() {
         return title;
